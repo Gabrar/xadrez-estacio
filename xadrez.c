32a4655f65a58d941343a60c39d586b391dbc621
Gabrar <gabriel2025diasss@gmail.com>
2025-04-22 21:25:08 +0000

    tema 3 desafio novato

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Movimento da Torre - 5 casas para a direita (usando for)
+    printf("Movimento da Torre:\n");
+    for(i = 1; i <= 5; i++) {
+        printf("Casa %d: Direita\n", i);
+    }
+
+    // Movimento do Bispo - 5 casas na diagonal para cima e à direita (usando while)
+    printf("\nMovimento do Bispo:\n");
+    i = 1;
+    while(i <= 5) {
+        printf("Casa %d: Cima, Direita\n", i);
+        i++;
+    }
+
+    // Movimento da Rainha - 8 casas para a esquerda (usando do-while)
+    printf("\nMovimento da Rainha:\n");
+    i = 1;
+    do {
+        printf("Casa %d: Esquerda\n", i);
+        i++;
+    } while(i <= 8);
+
+    return 0;
+}
 
